d62cb9535877713abb478f5857e34102371ed297
Remi Beucherie <beucherie.r@gmail.com>
2019-12-17 19:55:36 +0100

    try to add shopify api

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,11 @@
 class ProductsController < ApplicationController
+  require 'json'
+  require 'open-uri'
 
   def index
-    shop = ShopifyAPI::Shop.current
-
-    @products = ShopifyAPI::Product.find(:all)
+    url = 'https://734b93edaeb60470f95ef7240dc0b262:1ca54a7201761c040e09eac2b039324f@joone.myshopify.com/admin/api/2019-10/products.json'
+    products_serialized = open(url).read
+    products = JSON.parse(products_serialized)
+    raise
   end
 end
